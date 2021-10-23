06e6b2ffa409d62b9cc56a139230abd34c2a23cd
iadams749 <iwa332@email.unc.edu>
2021-10-23 13:27:20 -0400

    Initial Commit

---
+++ EnviroShop/EnviroShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnviroShopApp.swift
+//  EnviroShop
+//
+//  Created by Isaac Adams on 10/23/21.
+//
+
+import SwiftUI
+
+@main
+struct EnviroShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
