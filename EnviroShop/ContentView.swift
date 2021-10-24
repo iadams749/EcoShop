cc8452a7ee7ff9cc36606c244976d8a650dcd018
iadams749 <iwa332@email.unc.edu>
2021-10-24 02:01:04 -0400

    Barcode Scanner works

--- EnviroShop/ContentView.swift
+++ EnviroShop/ContentView.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import Foundation
+import CoreML
+import Vision
 
 
 //Loads the home screen
@@ -17,7 +20,7 @@ struct ContentView: View {
             
             
             
-            ZStack{
+            ZStack (alignment: .top){
                 
                 NavigationLink(destination: BarcodeView(), tag: "Barcode", selection: $selection ) {EmptyView()}
                 NavigationLink(destination: ProductIdView(), tag: "ProductId", selection: $selection ) {EmptyView()}
@@ -28,12 +31,10 @@ struct ContentView: View {
                 VStack{
                     
                     
-                    //App Label
-                    Text("EcoShop")
-                        .frame(width: 400, height: 60)
-                        .background(Color.egreen)
-                        .cornerRadius(10)
-                        .padding(.bottom, 20)
+                    //Top Banner
+                    Image("Banner")
+                        .resizable()
+                        .frame(width: 390, height: 100).padding(.bottom, 100)
                     
                     
                     //Search by label
@@ -70,20 +71,118 @@ struct ContentView: View {
                 }.padding(.bottom, 270)
                 
             }
+            .ignoresSafeArea(edges: .top)
         }
     }
 }
 
 struct BarcodeView: View {
     @State private var selection: String? = nil
+    @State var imageData: Data = .init(capacity: 0)
+    @State var show = false
+    @State var imagePicker = false
+    @State var source : UIImagePickerController.SourceType = .photoLibrary
+    
     
     var body: some View{
-        NavigationView{
-            ZStack{
-                //Background colors
-                LinearGradient(gradient: Gradient(colors: [.ecream,.white]), startPoint: .leading, endPoint: .bottom).ignoresSafeArea(edges: [.top, .bottom])
+        ZStack (alignment: .top){
+            //Background colors
+            LinearGradient(gradient: Gradient(colors: [.ecream,.white]), startPoint: .leading, endPoint: .bottom).ignoresSafeArea(edges: [.top, .bottom])
+            
+            NavigationLink(destination: ImagePicker(show: $imagePicker, image: $imageData, source: source), isActive: $imagePicker){
+                EmptyView()
+            }
+            
+            NavigationLink(destination: ProductIdView(), tag: "ProductId", selection: $selection ) {EmptyView()}
+            
+            VStack{
+                //Top Banner
+                Image("Banner")
+                    .resizable()
+                    .frame(width: 390, height: 100).padding(.bottom, 50)
+                
+                if imageData.count != 0 {
+                    Image(uiImage: UIImage(data: self.imageData)!)
+                        .resizable()
+                        .frame(width: 350, height: 350)
+                        .foregroundColor(Color.purple)
+                        //.rotationEffect(.degrees())
+                        .padding(.bottom, 20)
+                }
+                else{
+                    Image(systemName: "plus.square")
+                        .resizable()
+                        .frame(width: 350, height: 350)
+                        .foregroundColor(Color.egreen)
+                        .padding(.bottom, 20)
+                }
+                
+                Button(action: {
+                    self.show.toggle()
+                }){
+                    Text("Take a Photo")
+                        .frame(width: 250, height: 100, alignment: .center)
+                        .background(Color.eblue)
+                        .foregroundColor(Color.ecream)
+                        .cornerRadius(10)
+                }.actionSheet(isPresented: $show){
+                    ActionSheet(title: Text("Take a photo or select from photo library"), message: Text(""), buttons:
+                                    [.default(Text("Photo Library"), action: {
+                                        self.source = .photoLibrary
+                                        self.imagePicker.toggle()
+                                    }),.default(Text("Camera"), action: {
+                                        self.source = .camera
+                                        self.imagePicker.toggle()
+                                    })]
+                    )
+                }.padding(.bottom, 20)
+                
+                Button(action: {
+                    if imageData.count != 0{
+                        
+                        // Create a barcode detection-request
+                        let barcodeRequest = VNDetectBarcodesRequest(completionHandler: { request, error in
+                            
+                            guard let results = request.results else { return }
+                            
+                            print(results.count)
+                            
+                            // Loop through the found results
+                            for result in results {
+                                
+                                // Cast the result to a barcode-observation
+                                if let barcode = result as? VNBarcodeObservation {
+                                    
+                                    // Print barcode-values
+                                    print("Symbology: \(barcode.payloadStringValue!)")
+                                    
+                                    
+                                }
+                            }
+                            
+                        })
+                        
+                        // Create an image handler and use the CGImage your UIImage instance.
+                        guard let image = UIImage(data: self.imageData)!.cgImage else { return }
+                        let handler = VNImageRequestHandler(cgImage: image, options: [:])
+                        
+                        // Perform the barcode-request. This will call the completion-handler of the barcode-request.
+                        guard let _ = try? handler.perform([barcodeRequest]) else {
+                            return print("Could not perform barcode-request!")
+                        }
+                        
+                    }
+                }){
+                    Text("Submit Photo")
+                        .frame(width: 250, height: 100, alignment: .center)
+                        .background(Color.eblue)
+                        .foregroundColor(Color.ecream)
+                        .cornerRadius(10)
+                    
+                }
             }
         }
+        .ignoresSafeArea(edges: .top)
     }
 }
 
@@ -94,35 +193,64 @@ struct ProductIdView: View {
     @State private var errorLabel: String = ""
     
     var body: some View{
-        NavigationView{
-            ZStack (alignment: .top) {
-                //Background colors
-                LinearGradient(gradient: Gradient(colors: [.ecream,.white]), startPoint: .leading, endPoint: .bottom).ignoresSafeArea(edges: [.top, .bottom])
+        ZStack (alignment: .top) {
+            
+            NavigationLink(destination: BarcodeView(), tag: "Barcode", selection: $selection ) {EmptyView()}
+            
+            //Background colors
+            LinearGradient(gradient: Gradient(colors: [.ecream,.white]), startPoint: .leading, endPoint: .bottom).ignoresSafeArea(edges: [.top, .bottom])
+            
+            VStack{
                 
-                VStack{
-                    
-                    //Top Banner
-                    Image("Banner")
-                        .resizable()
-                        .frame(width: 390, height: 100).padding(.bottom, 100)
-                    
-                    Text("Enter Product ID:")
-                    TextField("Product",
-                              text: $id,
-                              onCommit:{
-                                let nums: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-                                
-                              })
-                        .frame(width: 300, height: 50)
-                        .background(Color.egreen)
-                        .foregroundColor(Color.black)
-                        .multilineTextAlignment(.center)
-                        .cornerRadius(10)
-                    Text(errorLabel)
-                }
+                //Top Banner
+                Image("Banner")
+                    .resizable()
+                    .frame(width: 390, height: 100).padding(.bottom, 100)
+                
+                Text("Enter Product ID:")
+                    .foregroundColor(Color.black)
+                TextField("Product",
+                          text: $id,
+                          onCommit:{
+                            var num = Int(id)
+                            
+                            if(num == nil){
+                                errorLabel = "Please enter a valid UPC";
+                            }
+                            else if(id.count != 12){
+                                errorLabel = "Please enter a valid UPC"
+                            }
+                            else{
+                                print(num!)
+                            }
+                          })
+                    .frame(width: 300, height: 50)
+                    .background(Color.egreen)
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                    .cornerRadius(10)
+                Text(errorLabel)
+                    .foregroundColor(Color.red)
+                    .fontWeight(.bold)
+                    .padding(.top, 20)
             }
             .ignoresSafeArea(edges: .top)
+            
+        }
+    }
+}
 
+struct ProfileView: View{
+    
+    var idString: String
+    var body: some View{
+
+        
+        ZStack(alignment: .top){
+            
+            //Background colors
+            LinearGradient(gradient: Gradient(colors: [.ecream,.white]), startPoint: .leading, endPoint: .bottom).ignoresSafeArea(edges: [.top, .bottom])
+            
         }
     }
 }
@@ -130,6 +258,49 @@ struct ProductIdView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductIdView()
+        ProfileView(idString: "01234565")
     }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var show : Bool
+    @Binding var image: Data
+    var source : UIImagePickerController.SourceType
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePicker.Coordinator(parent1: self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let controller = UIImagePickerController()
+        controller.sourceType = source
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>){
+        
+    }
+    
+    class Coordinator : NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        var parent : ImagePicker
+        init(parent1: ImagePicker){
+            parent = parent1
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            self.parent.show.toggle()
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            let image = info[.originalImage] as! UIImage
+            let data = image.pngData()
+            self.parent.image = data!
+            self.parent.show.toggle()
+            
+        }
+    }
+    
+    
 }
